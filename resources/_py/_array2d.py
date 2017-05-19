363d6abf61338b287046f80b8cd36714416bea40
Manish Devgan <manish.nsit8@gmail.com>
2017-05-19 13:49:10 +0530

    Update _array2d.py

--- resources/_py/_array2d.py
+++ resources/_py/_array2d.py
@@ -1,1 +1,19 @@
+# PYTHON
+# MANISH DEVGAN
+# https://github.com/gabru-md
 
+#BEGIN
+
+import numpy as np
+
+# a simple testing program to create a 2d Array
+
+a = [[[0,0,0] for x in range(int(width))] for y in range(height)]
+# after this we will input the fields that we created! 
+# code for that will be available in another Pytho file
+
+# converting the list type of 'a' into numpy.ndarray
+
+a = array(a) # or np.array(a) depending on what is right for you!
+
+#END
