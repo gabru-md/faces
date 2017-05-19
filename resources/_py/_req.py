9e4bacb6b7ed223ab9169d3b899617ade630fbda
Manish Devgan <manish.nsit8@gmail.com>
2017-05-19 13:58:36 +0530

    Create _req.py

---
+++ resources/_py/_req.py
@@ -0,0 +1,21 @@
+# PYTHON
+# MANISH DEVGAN
+# https://github.com/gabru-md
+
+#BEGIN
+
+#Containing import modules
+
+import sys
+
+import os
+
+import cv2
+
+import numpy as np
+
+import math
+
+import matplotlib.pyplot as plt
+
+#END
